09659dec7da371be1785393340acf2df1064344e
Alex <zkirpichz@mail.ru>
2023-04-26 08:47:50 +0500

    first commit

---
+++ Newspaper/users/models.py
@@ -0,0 +1,7 @@
+from msilib.schema import CustomAction
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(default=0)
